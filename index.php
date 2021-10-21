53c5d32bd7b5b899ebfddf88ffdfa85b292986f5
veronique <veroniqueaero@gmail.com>
2021-10-21 19:37:01 +0800

    modifier index.php

--- index.php
+++ index.php
@@ -14,31 +14,41 @@ $tUsers = $listeUsers->fetchAll(PDO::FETCH_ASSOC);
 <body>
    
     <article>
-    	<h1>la liste de tous les users</h1>
+    	<h1>la liste de tous les employes</h1>
+        <button>Ajouter</button>
     	<p>
-    	Nous avons actuellement <?php echo $listeUsers->rowCount(); ?> 
-    	Users<?php if($listeUsers->rowCount() > 1) echo 's'; ?>
+            Nous avons actuellement <?php echo $listeUsers->rowCount(); ?> 
+            employes<?php if($listeUsers->rowCount() > 1) echo 's'; ?>
         enregistre<?php if($listeUsers->rowCount() > 1) echo 's'; ?>	
         </p>
 
         <?php
-        //si on a au moins un film, on affiche la liste
+
           if($listeUsers->rowCount() > 0){ ?>
-          	<ul>
-          		<?php //on bouvle pour chaque film trouve
+
+            <table border="1">
+                <tr>
+                    <th>ID</th>
+                    <th>NOM</th>
+                    <th>PRENOM</th>
+                    <th>ACTION</th>
+                </tr>
+                
+                <?php
           		foreach ($tUsers as $value) {?>
-          			<li>
-          				<a>
-                    <?php echo $value['id_employes'];?>
-          					<?php echo $value['nom'];?>
-                              <?php echo $value['prenom'];?>
-          				</a>
-        
-          			</li>
+                  <tr>
+          			<td><?php echo $value['id_employes'];?></td>
+                    <td><?php echo $value['nom'];?></td>
+                    <td> <?php echo $value['prenom'];?></td>
+                    <td> <button>modifier</button> </td>
+                    </tr>
           		 <?php  }?>
-          	</ul>
+                
+            </table>
           
-         <?php } //fin du if ?>
+         <?php }?>
+
+         
 
     </article>
 </body>
